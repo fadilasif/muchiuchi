981f91e545d923c93d97594996c84b45aea4974d
Musheer0 <147691647+Musheer0@users.noreply.github.com>
2025-05-03 18:15:43 +0530

    style inpput

--- Project1.py
+++ Project1.py
@@ -1,6 +1,6 @@
 import random
 computer=random.choice([-1,0,1])
-youstr=input("Choose between snake, water and gun")
+youstr=input("Choose between snake, water and gun: ")
 youdict={"snake": -1,"water": 0,"gun": 1}
 if (youstr not in youdict):
     print("Invalid input. Try snake, water, or gun.")
